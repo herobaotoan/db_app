671615661fb026d74cd2bb38d83db97fe3453589
Chi Toan <88176211+herobaotoan@users.noreply.github.com>
2022-09-07 17:18:14 +0700

    Update customer.php

--- customer.php
+++ customer.php
@@ -5,16 +5,36 @@
 </div>
 <div class="row m-2">
     <div class="col-auto">
-    <form method="post" action="main.php">
-    <select name="PageNo">
-        <option selected value="1">1</option>
-        <option value="5">5</option>
-        <option value="10">10</option>
-        <option value="20">20</option>
-    </select>
-    <input type="submit" class="btn btn-sm btn-primary" name="load" value="Page">
-    </form>
-</div>
+        <form method="post" action="main.php">
+        <select name="PageNo">
+            <option selected value="1">1</option>
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="20">20</option>
+        </select>
+        <input type="submit" class="btn btn-sm btn-primary" name="PageNo" value="Page">
+        </form>
+    </div>
+    <div class="col-2">
+        <form method="post" action="main.php">
+            <input type="text" style="width: 50%" placeholder="Name" name="name">
+            <input type="submit" class="btn btn-sm btn-primary" name="search_name" value="Search">
+        </form>
+    </div>
+    <div class="col-3">
+        <form method="post" action="main.php">
+            <input type="number" style="width: 30%" placeholder="Price <=" name="price1">
+            <input type="number" style="width: 30%" placeholder="<= Price" name="price2">
+            <input type="submit" class="btn btn-sm btn-primary" name="search_price" value="Search">
+        </form>
+    </div>
+    <div class="col-3">
+        <form method="post" action="main.php">
+            <input type="text" style="width: 30%" placeholder="Key" name="key">
+            <input type="text" style="width: 30%" placeholder="Value" name="value">
+            <input type="submit" class="btn btn-sm btn-primary" name="search_attribute" value="Search">
+        </form>
+    </div>
 </div>
 
 
@@ -34,7 +54,7 @@
 <tbody>
 
 <?php
-$rows = $pdo->query("SELECT * FROM products ORDER BY Pid DESC LIMIT $PageNo;");
+$rows = $pdo->query("SELECT * FROM products $where $and ORDER BY Pid DESC LIMIT $PageNo;");
 //Descending order is to display *Most recently* added products first
     foreach ($rows as $row) {
         //Get product's status
@@ -48,7 +68,27 @@ $rows = $pdo->query("SELECT * FROM products ORDER BY Pid DESC LIMIT $PageNo;");
     }
     $Pname = $row['Pname'];
     $price = $row['Price'];
-    echo<<<GFG
+    //Get product's extra attributes
+    $attributeCheck = false; //For search attribute engine
+    $keys = array();
+    $values = array();
+    $document = $collection->find()->toArray();
+    for ($i = 0; $i < count($document); $i++){
+        if ($document[$i]['Pid'] == $id){
+            $newly_added = false;
+            $count = 0;
+            foreach($document[$i] as $key => $value)
+            {
+                if ($count > 1){
+                    array_push($keys, $key);
+                    array_push($values, $value);
+                    if ($key == $keySearch && $value == $valueSearch){ $attributeCheck = true; } //For search attribute engine
+                } $count++;
+            }
+        }
+    }
+    if (!$attributeSearch || ($attributeSearch && $attributeCheck)){
+        echo<<<GFG
             <tr>
             <form method="post" action="main.php">
                 <td>
@@ -63,23 +103,14 @@ $rows = $pdo->query("SELECT * FROM products ORDER BY Pid DESC LIMIT $PageNo;");
                 </td>
                 <td><input type="submit" name="C" class="btn btn-success" value="Buy"></td>
             </form>
-    GFG;
-    $document = $collection->find()->toArray();
-    for ($i = 0; $i < count($document); $i++){
-        if ($document[$i]['Pid'] == $id){
-            $newly_added = false;
-            $count = 0;
-            foreach($document[$i] as $key => $value)
-            {
-                if ($count > 1){ //Skip the first 2 keys and values (which is _id an Pid)
-                    echo<<<GFG
-                    <td>$key: $value</td>
-                    GFG;
-                } $count++;
-            }
+        GFG;
+        for ($i = 0; $i < count($keys); $i++){
+            $key = $keys[$i];
+            $value = $values[$i];
+            echo "<td>$key: $value</td>";
         }
+        echo "</tr>";
     }
-    echo "</tr>";
 }
 echo "</tbody></table>";
 
